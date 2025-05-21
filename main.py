6569430cada358eecf4bf0061f0e2ad83200991c
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-05-21 20:29:13 -0300

    Inserindo colunas, imagem e descrição

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg", width=350)
+
+with col2:
+    st.title("Matheus Santos")
+    texto = """
+
+    Olá! Eu sou um programador Python, estudante de inglês e ciencia da computação. Estudo programação desde 
+    2018 e possuo experiencia profissional solida com rotinas administrativas com Excel e VBA onde atuo a 3 anos.
+
+    """
+    st.write(texto)
