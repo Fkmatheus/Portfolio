b6132c3ca3ed2e474dbe10c3c96d92656a06bd41
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-05-21 21:32:53 -0300

    Realizando ajustes nas colunas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty, col2 = st.columns([1, 0.2, 2])
 
 with col1:
     st.image("images/photo.jpg", width=400)
@@ -23,19 +23,19 @@ with col2:
     st.write("Linkedin: https://www.linkedin.com/in/matheus-santos-aa31a823a/")
 
 
-col3, col4 = st.columns(2)
+col3, empty2, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-        st.image(f"images/{row["image"]}")
         st.write(row["description"])
+        st.image(f"images/{row["image"]}")
         st.write(row["url"])
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-        st.image(f"images/{row["image"]}")
         st.write(row["description"])
+        st.image(f"images/{row["image"]}")
         st.write(row["url"])
